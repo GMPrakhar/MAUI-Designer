525744bc7d813d503c58250f384abd86b765a834
plondhe <plondhe@WIN-N034943OVRS>
2024-07-25 14:04:55 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIDesigner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Platform;
+using System.Runtime.CompilerServices;
+using Extensions = Microsoft.Maui.Controls.Xaml.Extensions;
+
+namespace MAUIDesigner
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public IEnumerable<string> ElementItems { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RenderXAML(object sender, EventArgs e)
+        {
+            var xaml = Editor.Text;
+            try
+            {
+                Extensions.LoadFromXaml(Shower, xaml);
+            }
+            catch (Exception)
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "Invalid XAML", "OK");
+            }
+        }
+    }
+
+}
