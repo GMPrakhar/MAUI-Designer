a91ccd7ef6a81f22250d01ff348b2cf4dc878582
Prakhar Londhe <prakhar.londhe30@gmail.com>
2024-07-30 22:25:34 +0530

    Added basic toolkit section and drag drop support. Also added basic xaml generation visible in debug mode.

---
+++ ElementCreator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIDesigner
+{
+    internal static class ElementCreator
+    {
+        internal static View Create(string elementTypeName)
+        {
+            // Get the class that extends View with the name as elementType
+            var elementType = typeof(View).Assembly.GetTypes().FirstOrDefault(t => t.Name == elementTypeName);
+            var newElement = Activator.CreateInstance(elementType) as View;
+            newElement.Margin = new Thickness(20);
+            if(newElement is Label)
+            {
+                (newElement as Label).Text = "Drag me!";
+            }
+            else if(newElement is Editor)
+            {
+                var element = newElement as Editor;
+                element.Text = "Type here";
+                // disable editor text edit
+                element.IsEnabled = false;
+            }
+
+            return newElement;
+        }
+    }
+}
