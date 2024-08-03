801894684bc6820a4ba8b6c9e9b22c9c6757a978
Prakhar Londhe <prakhar.londhe30@gmail.com>
2024-08-03 14:37:31 +0530

    Added propertes section for selected element that can be tweaked to update the element directly.

--- Designer.xaml.cs
+++ Designer.xaml.cs
@@ -10,20 +10,8 @@ public partial class Designer : ContentPage
 {
     private bool isDragging = false;
     private View? draggingView;
-    Border gradientBorder = new Border
-    {
-        StrokeThickness = 2,
-        ZIndex = -10,
-        Padding = new Thickness(20,0),
-        HorizontalOptions = LayoutOptions.Center,
-        StrokeShape = new RoundRectangle
-        {
-            CornerRadius = new CornerRadius(2)
-        },
-        Stroke = new SolidColorBrush(Colors.White),
-        InputTransparent = true,
-        IsEnabled = false
-    };
+    private View? focusedView;
+    private Rectangle? scalerRect;
     private const string LabelXAML = "<Label Text=\"Drag Me away\" HorizontalTextAlignment=\"Center\" TextColor=\"White\" FontSize=\"36\">\r\n</Label>";
     private IDictionary<Guid, View> views = new Dictionary<Guid, View>();
 
@@ -46,7 +34,6 @@ public partial class Designer : ContentPage
             label.GestureRecognizers.Add(gestureRecognizer);
             Toolbox.Children.Add(label);
         }
-        designerFrame.Add(gradientBorder);
     }
 
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
@@ -58,10 +45,8 @@ public partial class Designer : ContentPage
         try
         {
             var newElement = ElementCreator.Create((sender as Label).Text);
-            var gestureRecognizer = new PointerGestureRecognizer();
-            gestureRecognizer.PointerEntered += AddBorder;
-            gestureRecognizer.PointerExited += RemoveBorder;
-            newElement.GestureRecognizers.Add(gestureRecognizer);
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += EnableElementForOperations;
             designerFrame.Add(newElement);
             views.Add(newElement.Id, newElement);
 
@@ -74,7 +59,12 @@ public partial class Designer : ContentPage
             // Do nothing
         }
     }
-
+    private void EnableElementForOperations(object? sender, TappedEventArgs e)
+    {
+        var senderView = sender as View;
+        draggingView = senderView;
+        AddBorder(senderView, null);
+    }
 
     public string GetXamlForElement(VisualElement element)
     {
@@ -120,7 +110,7 @@ public partial class Designer : ContentPage
 
     private void RemoveBorder(object? sender, PointerEventArgs e)
     {
-        gradientBorder.Opacity = 0;
+        gradientBorder2.Opacity = 0;
         if(draggingView != null) return;
     }
 
@@ -128,14 +118,13 @@ public partial class Designer : ContentPage
     {
         try
         {
-            if (draggingView != null) return;
             View? senderView = (sender as View);
             var location = new Point(senderView.X, senderView.Y);
 
-            gradientBorder.HeightRequest = senderView.Height;
-            gradientBorder.WidthRequest = senderView.Width;
-            gradientBorder.Opacity = 1;
-            gradientBorder.Margin = new Thickness(senderView.X, senderView.Y);
+            gradientBorder2.HeightRequest = senderView.Height;
+            gradientBorder2.WidthRequest = senderView.Width;
+            gradientBorder2.Opacity = 1;
+            gradientBorder2.Margin = new Thickness(senderView.X, senderView.Y);
         }catch(Exception)
         {
 
@@ -144,14 +133,21 @@ public partial class Designer : ContentPage
 
     private void PointerGestureRecognizer_PointerMoved(object sender, PointerEventArgs e)
     {
-        if(!isDragging || draggingView == null) return;
-
         Point location = e.GetPosition(designerFrame).Value;
-        gradientBorder.Opacity = 1;
+        //if (focusedView != null && isDragging)
+        //{
+        //    focusedView.HeightRequest += focusedView.Y - location.Y;
+        //    focusedView.WidthRequest += location.X - focusedView.X;
+        //    focusedView.Margin = new Thickness(location.X, location.Y);
+        //}
+
+        if (!isDragging || draggingView == null) return;
+
+        gradientBorder2.Opacity = 1;
         draggingView.Margin = new Thickness(location.X, location.Y);
-        gradientBorder.Margin = draggingView.Margin;
-        gradientBorder.HeightRequest = draggingView.Height;
-        gradientBorder.WidthRequest = draggingView.Width;
+        gradientBorder2.Margin = draggingView.Margin;
+        gradientBorder2.HeightRequest = draggingView.Height;
+        gradientBorder2.WidthRequest = draggingView.Width;
     }
 
     private void PointerGestureRecognizer_PointerPressed(object sender, PointerEventArgs e)
@@ -160,18 +156,89 @@ public partial class Designer : ContentPage
         Point location = e.GetPosition(designerFrame).Value;
 
         // Get all children of designerFrame and find the element that is being dragged
-        var childElements = designerFrame.Children.Where(c => c.Frame.Contains(location) && c is not Border);
+        var childElements = designerFrame.Children.Where(c => c.Frame.Contains(location) && c != gradientBorder2);
 
-        draggingView = (View)childElements.FirstOrDefault();
+
+        var firstChild = (View)childElements.FirstOrDefault();
+        if (firstChild is Rectangle)
+        {
+            scalerRect = firstChild as Rectangle;
+            var nonRectFirstChild = childElements.Where(c => c != firstChild).FirstOrDefault();
+            if (nonRectFirstChild != null)
+            {
+                draggingView = nonRectFirstChild as View;
+            }
+        }
+        else
+        {
+            draggingView = firstChild;
+        }
+
+        if (draggingView != null)
+        {
+            AddBorder(draggingView, null);
+            focusedView = draggingView;
+            // Get properties for the focused view and display it in the properties panel
+            var properties = ToolBox.GetAllPropertiesForView(focusedView);
+            Properties.Children.Clear();
+
+            foreach (var property in properties)
+            {
+                var label = new Label
+                {
+                    Text = property.Key,
+                    FontSize = 10,
+                    TextColor = Colors.White,
+                    BackgroundColor = Colors.Transparent,
+                };
+                var value = property.Value;
+                // Put the label and value in a grid layout
+                var grid = new Grid()
+                {
+                    ColumnDefinitions = new ColumnDefinitionCollection
+                    {
+                        new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) }
+                    },
+                    HeightRequest = 30,
+                    Margin = new Thickness(0, 0, 0, 10),
+                    InputTransparent = true,
+                    CascadeInputTransparent = false,
+                };
+
+                grid.Children.Add(label);
+                grid.Children.Add(value);
+
+                grid.SetColumn(label, 0);
+                grid.SetColumn(value, 1);
+
+
+                Properties.Children.Add(grid);
+            }
+        }
+        else
+        {
+            focusedView = null;
+            RemoveBorder(draggingView, null);
+        }
     }
 
     private void PointerGestureRecognizer_PointerReleased(object sender, PointerEventArgs e)
     {
         if (!isDragging || draggingView == null) return;
         isDragging = false;
-        RemoveBorder(draggingView, null);
+        //RemoveBorder(draggingView, null);
         draggingView = null;
+        scalerRect = null;
     }
 
+    private void DragGestureRecognizer_DragStarting(object? sender, DragStartingEventArgs e)
+    {
+        scalerRect = sender as Rectangle;
+    }
 
+    private void DragGestureRecognizer_DropCompleted(object? sender, DropCompletedEventArgs e)
+    {
+        scalerRect = null;
+    }
 }
